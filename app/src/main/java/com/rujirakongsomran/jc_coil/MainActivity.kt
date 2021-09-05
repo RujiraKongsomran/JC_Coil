f50c63217925dc14125b13340409735a030064e7
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-05 21:18:35 +0700

    Load image from url

--- app/src/main/java/com/rujirakongsomran/jc_coil/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_coil/MainActivity.kt
@@ -3,11 +3,15 @@ package com.rujirakongsomran.jc_coil
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.rujirakongsomran.jc_coil.ui.theme.JC_CoilTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,14 +19,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_CoilTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CoilImage()
                 }
             }
         }
     }
 }
+
+@Composable
+fun CoilImage() {
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        val painter = rememberImagePainter(
+            data = "https://cdn.pixabay.com/photo/2021/08/28/18/09/common-heather-6581569_960_720.jpg",
+            builder = {
+
+            }
+        )
+        Image(painter = painter, contentDescription = "Logo Image")
+    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
@@ -33,6 +60,12 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JC_CoilTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CoilImage()
+        }
     }
 }
